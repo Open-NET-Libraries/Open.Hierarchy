cbe2352ca5cba383991375574a883e0db1f26591
electricessence <5899455+electricessence@users.noreply.github.com>
2022-02-24 16:49:31 -0800

    Null assertion fixes.

--- IChild.cs
+++ IChild.cs
@@ -1,38 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
-namespace Open.Hierarchy
+namespace Open.Hierarchy;
+
+public interface IChild
 {
-	public interface IChild
-	{
-		/// <summary>
-		/// The parent of this child.
-		/// </summary>
-		object? Parent { get; }
-	}
+	/// <summary>
+	/// The parent of this child.
+	/// </summary>
+	object? Parent { get; }
+}
 
-	public interface IChild<out TParent> : IChild
-		where TParent : class
-	{
-		/// <summary>
-		/// The generic parent of this child.
-		/// </summary>
-		new TParent? Parent { get; }
-	}
+public interface IChild<out TParent> : IChild
+	where TParent : class
+{
+	/// <summary>
+	/// The generic parent of this child.
+	/// </summary>
+	new TParent? Parent { get; }
+}
 
-	public static class ChildExtensions
+public static class ChildExtensions
+{
+	/// <summary>
+	/// Crawls the ancestor lineage and returns them.
+	/// </summary>
+	/// <typeparam name="TNode">The node type.</typeparam>
+	/// <param name="node">The child node to use.</param>
+	/// <returns>An enumerable of the ancestors.</returns>
+	public static IEnumerable<TNode> GetAncestors<TNode>(
+		this TNode node)
+		where TNode : class, IChild<TNode>
 	{
-		/// <summary>
-		/// Crawls the ancestor lineage and returns them.
-		/// </summary>
-		/// <typeparam name="TNode">The node type.</typeparam>
-		/// <param name="node">The child node to use.</param>
-		/// <returns>An enumerable of the ancestors.</returns>
-		public static IEnumerable<TNode> GetAncestors<TNode>(
-			this TNode node)
-			where TNode : class, IChild<TNode>
+		if (node is null) throw new ArgumentNullException(nameof(node));
+		Contract.EndContractBlock();
+
+		return GetAncestorsCore(node);
+
+		static IEnumerable<TNode> GetAncestorsCore(TNode node)
 		{
-			if (node is null) throw new ArgumentNullException(nameof(node));
 			TNode? parent;
 			while ((parent = node.Parent) != null)
 			{
@@ -40,26 +47,24 @@ namespace Open.Hierarchy
 				node = parent;
 			}
 		}
-
-
-		/// <summary>
-		/// Crawls the ancestor lineage returns the first node with no parent.
-		/// </summary>
-		/// <typeparam name="TNode">The node type.</typeparam>
-		/// <param name="node">The child node to start with.</param>
-		/// <returns>The root node.</returns>
-		public static TNode GetRoot<TNode>(
-			this TNode node)
-			where TNode : class, IChild<TNode>
-		{
-			if (node is null) throw new ArgumentNullException(nameof(node));
-			TNode? parent;
-			while ((parent = node.Parent) != null)
-			{
-				node = parent;
-			}
-			return node;
-		}
 	}
 
+	/// <summary>
+	/// Crawls the ancestor lineage returns the first node with no parent.
+	/// </summary>
+	/// <typeparam name="TNode">The node type.</typeparam>
+	/// <param name="node">The child node to start with.</param>
+	/// <returns>The root node.</returns>
+	public static TNode GetRoot<TNode>(
+		this TNode node)
+		where TNode : class, IChild<TNode>
+	{
+		if (node is null) throw new ArgumentNullException(nameof(node));
+		TNode? parent;
+		while ((parent = node.Parent) != null)
+		{
+			node = parent;
+		}
+		return node;
+	}
 }
